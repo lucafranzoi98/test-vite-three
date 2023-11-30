63105c1635ff1f19c04156f6ec826511356bb579
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-11-30 11:47:53 +0100

    add scene

--- src/main.js
+++ src/main.js
@@ -1,5 +1,33 @@
 import { createApp } from 'vue'
 import './scss/app.scss'
 import App from './App.vue'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshNormalMaterial();
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    controls.update();
+
+    renderer.render(scene, camera);
+}
+
+animate();
 
 createApp(App).mount('#app')
