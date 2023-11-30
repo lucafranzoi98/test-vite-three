c802cde808486aaae5152922503bc393724f0967
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-11-30 11:12:09 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="text-center fs-1 py-4 text-light">
+        test
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
